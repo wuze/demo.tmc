0a889d7f46ba62e7bbbb7cec52dc9165e1584eaf
XiaoWu <xiaowoo_123@163.com>
2017-07-14 14:01:07 +0800

    提交

---
+++ vue_demo/src/App.vue
@@ -0,0 +1,55 @@
+
+<template>
+<div>
+<Contents v-if="isShow_home"></Contents>
+<Role v-if="isShow_menu" ></Role>
+<Mine v-if="isShow_mine"></Mine>
+<Menus v-on:show-what="geet"></Menus> 
+</div>
+</template>
+
+<script >
+import Menus from './components/Menus'
+import Contents from './components/Contents'
+import Role from './components/Role'
+import Mine from './components/Mine'
+ export default {
+  components: {
+  	Menus,Contents,Role,Mine
+  },
+  data () {
+   return{
+   	isShow_home:true,
+    isShow_menu:false,
+    isShow_mine:false
+    
+   }
+  },methods: {
+    geet:function(msg) {
+     if (msg=="home") {
+       this.isShow_home=true;
+       this.isShow_menu=false;
+       this.isShow_mine=false;
+     }else if(msg=="menu"){
+       this.isShow_home=false;
+       this.isShow_menu=true;
+       this.isShow_mine=false;
+     }
+     else if(msg=="mine"){
+       this.isShow_home=false;
+       this.isShow_menu=false;
+       this.isShow_mine=true;
+     }
+
+      var  _self=this;
+      function aaa() {
+      console.log(_self.isShow_home);
+      }
+       aaa();
+    }
+    
+  }
+
+} 
+
+</script>
---
+++ vue_demo/src/components/Mine.vue
@@ -0,0 +1,36 @@
+<template>
+   <div>
+   	 <img id="code" src="../img/code.png">
+   	 <div id="note" v-once>{{msg}}<div id="h"></div></div>
+   	 
+   </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '这个是我个人建的群,自己平时琢磨的东西都会发群里,现在略懂angular,vue,react等等,热爱编程,如果有志同道合的朋友可以加入,大家一起研究',
+    }
+  }
+}
+</script>
+
+<style >
+#code{
+	width: 80%;
+	margin:10px 10%;
+
+}
+#note{
+	width: 80%;
+	line-height: 35px;
+	margin: 10px auto;
+
+}
+ #h{
+ 	width: 100%;
+ 	height: 60px;
+ }
+</style>
